a3f3f43ede5567782bc20031919f15819c1a22ee
Thaix2phomai <93390718+Thaix2phomai@users.noreply.github.com>
2022-01-21 15:21:40 +0700

    baitapvenha

---
+++ BTVN/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import Controller.Controller;
+import database.ConnectJDBC;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        Controller co = new Controller();
+        co.MainMenu();
+    }
+}
